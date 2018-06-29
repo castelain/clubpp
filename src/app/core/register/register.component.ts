ec1e5271b9aa126766b95075b2afff9ddee6230a
Rabbit-A512 <2654525303@qq.com>
2018-06-30 02:17:24 +0800

    implement register form

--- src/app/core/register/register.component.ts
+++ src/app/core/register/register.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators
+} from '@angular/forms';
+import { format } from 'date-fns';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +13,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  validateForm: FormGroup;
 
-  constructor() { }
+  submitForm(): void {
+    const bd = this.validateForm.get('day_of_birth').value;
+    console.log(format(bd, 'YYYY-MM-DD'));
+    // for (const i in this.validateForm.controls) {
+    //   this.validateForm.controls[ i ].markAsDirty();
+    //   this.validateForm.controls[ i ].updateValueAndValidity();
+    // }
+    for (const field of Object.keys(this.validateForm.controls)) {
+      this.validateForm.controls[field].markAsDirty();
+      this.validateForm.controls[field].updateValueAndValidity();
+    }
+  }
 
-  ngOnInit() {
+  updateConfirmValidator(): void {
+    /** wait for refresh value */
+    Promise.resolve().then(() => this.validateForm.controls.checkPassword.updateValueAndValidity());
+  }
+
+  confirmationValidator = (control: FormControl): { [s: string]: boolean } | null => {
+    if (!control.value) {
+      return { required: true };
+    } else if (control.value !== this.validateForm.controls.password.value) {
+      return { confirm: true, error: true };
+    }
+  }
+
+  getCaptcha(e: MouseEvent): void {
+    e.preventDefault();
+  }
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      username: [null, [Validators.required]],
+      email: [null, [Validators.email]],
+      password: [null, [Validators.required]],
+      checkPassword: [null, [Validators.required, this.confirmationValidator]],
+      name: [null, [Validators.required]],
+      phonePrefix: ['+86'],
+      phone: [null, [Validators.required]],
+      agree: [false],
+      day_of_birth: [Date(), [Validators.required]],
+      academy: [null, [Validators.required]],
+      major: [null, [Validators.required]],
+      bio: [null],
+      year_of_enrollment: [null, [Validators.required]],
+      gender: [true]
+    });
   }
 
 }
